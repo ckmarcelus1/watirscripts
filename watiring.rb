ed840d8a516acf6c3ffac3594f3826f574f3b1f6
ckmarcelus1 <ckmarcelus@gmail.com>
2013-10-22 09:52:12 -0500

    added some changes to watiring

--- watiring.rb
+++ watiring.rb
@@ -6,12 +6,36 @@ require 'watir-webdriver'
 class Magento_GuestAddToCart < MiniTest::Unit::TestCase
     def test_functionality
         browser = Watir::Browser.new :chrome
-        browser.goto("http://www.containerstore.com/locations/index.htm")
-        browser.link(:class => "monetize_lightbox_close_0").click
-        browser.link(:href, "http://www.containerstore.com/locations/showStore.htm?store=GAL").click
-        browser.text_field(:id, "zipCode").set("11225")
-        browser.button(:title, "searchbutton").click
-        browser.button(:title, "58th & Lexington").click
+        browser.goto("http://demo.magentocommerce.com/electronics")
+        browser.goto("http://demo.magentocommerce.com/electronics/cellphones")
+        browser.link(:href, "http://demo.magentocommerce.com/samsung-mm-a900m-ace.html").click
+        browser.text_field(:id, "qty").set("3")
+        browser.button(:title, "Add to Cart").click
+        browser.button(:title, "Proceed to Checkout").click
+        browser.radio(:id, "login:guest").set
+        browser.div(:id, "checkout-step-login").button(:text, "Continue").click
+        browser.text_field(:id, "billing:firstname").set("John")
+        browser.text_field(:id, "billing:lastname").set("Doe")
+        browser.text_field(:id, "billing:company").set("My company name here")
+        browser.text_field(:id, "billing:email").set("johndoe@gmail.com")
+        browser.text_field(:id, "billing:street1").set("Sample street name1")
+        browser.text_field(:id, "billing:street2").set("Please ignore this order, developer test")
+        browser.select_list(:id, "billing:region_id").set("California")
+        browser.text_field(:id, "billing:city").set("Alamo")
+        browser.text_field(:id, "billing:postcode").set("94507")
+        browser.select_list(:id, "billing:country_id").set("United States")
+        browser.text_field(:id, "billing:telephone").set("0038531399222")
+        browser.div(:id, "checkout-step-billing").radio(:id, "billing:use_for_shipping_yes").set
+        browser.button(:title, "Continue").click
+        sleep 4
+        browser.radio(:id, "s_method_flatrate_flatrate").set
+        browser.div(:id, "checkout-step-shipping_method").button(:text, "Continue").click
+        sleep 4
+        browser.radio(:id, "p_method_checkmo").set
+        browser.div(:id, "checkout-step-payment").button(:text, "Continue").click
+        sleep 4
+        browser.div(:id, "checkout-step-review").button(:text, "Place Order").click
+        #browser.close
        
     end
 end
